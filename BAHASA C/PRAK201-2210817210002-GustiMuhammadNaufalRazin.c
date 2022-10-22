2384019a70aad44aa132eb2cb61ca271d20b8228
GustiMuhammadNaufalRazin <naufal190903@gmail.com>
2022-10-22 20:27:53 +0800

    first commit

---
+++ BAHASA C/PRAK201-2210817210002-GustiMuhammadNaufalRazin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    char a[10],b[10],c[10],d[10],e[10],f[10],g[10];
+
+    printf("Nama                    : ");
+    gets(a);
+    printf("NIM                 	: ");
+    gets(b);
+    printf("Kelas Paralel	        : ");
+    gets(c);
+    printf("Tempat/Tanggal Lahir	: ");
+    gets(d);
+    printf("Alamat	                : ");
+    gets(e);
+    printf("Hobby               	: ");
+    gets(f);
+    printf("No. HP                  : ");
+    gets(g);
+    return 0;
+}
